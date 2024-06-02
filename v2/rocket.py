c64ef58948082bd690ee5113a5729be43cde06cd
Joey Marcinowski <66542880+Pixels128@users.noreply.github.com>
2024-06-02 00:47:37 -0400

    cleaned up rocket.py + added docs

--- v2/rocket.py
+++ v2/rocket.py
@@ -1,10 +1,12 @@
-from globals import unit, Quantity, Q
+"""Rocket class file"""
 from dataclasses import dataclass
+from globals import Quantity, Q
 from object import Object
+
 
 @dataclass
 class Rocket(Object):
+    """Rocket Object. Includes thrust, burn_time, and max_dy on top of the Object attributes"""
     thrust:Quantity = 0
     burn_time:Quantity = 0
     max_dy:Quantity = Q(0, "meter / second")
-    
